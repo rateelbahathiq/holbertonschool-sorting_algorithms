986e06c8753df66d6bc5f72bd407cb85f3b3424e
rateelbahathiq <rateelbahathiq@gmail.com>
2025-10-27 15:23:02 +0000

    add task 2

--- print_array.c
+++ print_array.c
@@ -4,17 +4,17 @@
 /**
  * print_array - Prints an array of integers
  * @array: The array to be printed
- * @size: Number of elements in the array
+ * @size: Number of elements in @array
  */
 void print_array(const int *array, size_t size)
 {
-    size_t i;
+	size_t i;
 
-    for (i = 0; i < size; i++)
-    {
-        if (i > 0)
-            printf(", ");
-        printf("%d", array[i]);
-    }
-    printf("\n");
+	for (i = 0; array && i < size; i++)
+	{
+		if (i > 0)
+			printf(", ");
+		printf("%d", array[i]);
+	}
+	printf("\n");
 }
